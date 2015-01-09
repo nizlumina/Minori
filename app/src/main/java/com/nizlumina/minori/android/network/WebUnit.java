2caea2436275e8a74a86b9235076aad2f75ad2b8
Nizlumina <nizlumina@gmail.com>
2015-01-10 01:16:00 +0800

    Added more comments and enqueing string request to Webunit

--- app/src/main/java/com/nizlumina/minori/android/network/WebUnit.java
+++ app/src/main/java/com/nizlumina/minori/android/network/WebUnit.java
@@ -15,6 +15,7 @@ package com.nizlumina.minori.android.network;
 import android.content.Context;
 
 import com.squareup.okhttp.Cache;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -28,7 +29,7 @@ import java.io.InputStream;
 class WebUnitMaster
 {
     private static volatile WebUnitMaster INSTANCE = null;
-    final OkHttpClient okHttpClient;
+    private final OkHttpClient okHttpClient;
 
     private WebUnitMaster(Context context)
     {
@@ -71,7 +72,8 @@ class WebUnitMaster
  * Otherwise, instances are thread-safe.
  * <p>
  * Any WebUnit calls should be wrapped in a worker thread.
- * Enqueuing request will be added later as needed (it will follow this <a href="http://stackoverflow.com/a/23965563/3939904">link</a>)
+ * Enqueuing any other request apart from String request will be added later as needed
+ * (it will follow this <a href="http://stackoverflow.com/a/23965563/3939904">link</a>)
  * </p>
  */
 public class WebUnit
@@ -79,9 +81,17 @@ public class WebUnit
     private static final String userAgentKey = "User-Agent";
     private static final String userAgent = "minori-android";
 
-    public synchronized String getString(Context context, String url) throws IOException
+    /**
+     * Get a string object from response body of the given url. For threaded calls, use {@link #enqueueGetString}
+     *
+     * @param context Any applicable context for the initialization
+     * @param url     The URL for the request
+     * @return The body in a string object
+     * @throws IOException
+     */
+    public synchronized String getString(final Context context, final String url) throws IOException
     {
-        final Response response = initRequestAndExecute(context, url);
+        final Response response = executeSync(context, url);
 
         if (response != null && response.isSuccessful())
         {
@@ -90,9 +100,17 @@ public class WebUnit
         return null;
     }
 
-    public synchronized void invokeOnStream(Context context, String url, StreamCallable callable) throws IOException
+    /**
+     * Invoke methods on a returned Stream via a Callable
+     *
+     * @param context  Any applicable context for the initialization
+     * @param url      The URL for the request
+     * @param callable The callable instance to apply on the fully received inputstream
+     * @throws IOException
+     */
+    public synchronized void invokeOnStream(final Context context, final String url, final StreamCallable callable) throws IOException
     {
-        final Response response = initRequestAndExecute(context, url);
+        final Response response = executeSync(context, url);
 
         if (response != null && response.isSuccessful())
         {
@@ -100,14 +118,55 @@ public class WebUnit
         }
     }
 
-    private Response initRequestAndExecute(Context context, String url) throws IOException
+    private Response executeSync(final Context context, final String url) throws IOException
     {
-        final Request request = new Request.Builder()
-                .url(url)
-                .addHeader(userAgentKey, userAgent).build();
+        final Request request = getFormattedRequest(url);
         return WebUnitMaster.getInstance(context).getClient().newCall(request).execute();
     }
 
+    private Request getFormattedRequest(String url)
+    {
+        return new Request.Builder()
+                .url(url)
+                .addHeader(userAgentKey, userAgent).build();
+    }
+
+    /**
+     * Enqueue a request to the given URL and receive the response body in a String object via a listener
+     *
+     * @param context  Any applicable context for the initialization
+     * @param url      The URL for the request
+     * @param listener Optional listener to retrieve the string object of the received response body
+     * @throws IOException
+     */
+    public synchronized void enqueueGetString(final Context context, final String url, final WebUnitListener listener) throws IOException
+    {
+        executeAsync(context, url, new Callback()
+        {
+            @Override
+            public void onFailure(Request request, IOException e)
+            {
+                if (listener != null) listener.onFailure();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException
+            {
+                if (listener != null) listener.onFinish(response.body().toString());
+            }
+        });
+    }
+
+
+    private void executeAsync(final Context context, final String url, final Callback internalCallback) throws IOException
+    {
+        final Request request = getFormattedRequest(url);
+        WebUnitMaster.getInstance(context).getClient().newCall(request).enqueue(internalCallback);
+    }
+
+    /**
+     * A simple specialized callable interface for WebUnit stream responses
+     */
     public interface StreamCallable
     {
         /**
@@ -116,5 +175,23 @@ public class WebUnit
          * @param inputStream The full InputStream received
          */
         public void onStreamReceived(InputStream inputStream);
+    }
+
+    /**
+     * A simple listener interface for any WebUnit asynchronous operations
+     */
+    public interface WebUnitListener
+    {
+        /**
+         * Fires if the request failed in some way
+         */
+        public void onFailure();
+
+        /**
+         * Fires upon the completion of the request
+         *
+         * @param responseBody A string object of the response body
+         */
+        public void onFinish(String responseBody);
     }
 }
