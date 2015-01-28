0fbae46d6b3a6a3a194c90fe488dee7e9b41c703
Nizlumina <nizlumina@gmail.com>
2015-01-28 19:21:24 +0800

    Initial base class

---
+++ app/src/main/java/com/nizlumina/minori/android/fragment/SeasonFragment.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Nizlumina Studio (Malaysia)
+ *
+ * Unless specified, permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.nizlumina.minori.android.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nizlumina.minori.R;
+
+public class SeasonFragment extends Fragment
+{
+    Toolbar mToolbar;
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+        mToolbar = (Toolbar) activity.findViewById(R.id.base_toolbar);
+        if (mToolbar != null) mToolbar.setVisibility(View.GONE);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_gridview, container, false);
+
+        //load stuffs
+        return view;
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+        if (mToolbar != null) mToolbar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+    }
+}
