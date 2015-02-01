969b244ad306c516a01e304f1974ced5948576eb
Nizlumina <nizlumina@gmail.com>
2015-02-02 01:22:15 +0800

    Initial base import.

--- app/src/main/java/com/nizlumina/minori/android/fragment/SetupFragment.java
+++ app/src/main/java/com/nizlumina/minori/android/fragment/SetupFragment.java
@@ -12,6 +12,43 @@
 
 package com.nizlumina.minori.android.fragment;
 
-public class SetupFragment
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SetupFragment extends Fragment
 {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+    }
 }
